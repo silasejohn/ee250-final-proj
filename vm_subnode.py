7e7ac1d4bed789adde47d5a4b7ed5d40c4797998
Silas John <sejohn@usc.edu>
2022-05-02 18:18:22 +0000

    code for serial ID initialization

--- vm_subnode.py
+++ vm_subnode.py
@@ -4,10 +4,23 @@ Run vm_subscriber-maxcProj.py in a separate terminal on your VM."""
 import paho.mqtt.client as mqtt
 import time
 
-def on_connect(client, userdata, flags, rc):
-    print("Connected to server (i.e., broker) with result code "+str(rc))
+#################
+## Global Vars ##
+#################
+serialID = 0
+serialID_gen = "masterNode/serialID"
+serialIDACK = "masterNode/serialIDACK"
+isSerialOpenForInit = True
 
-    #s ubscribe to the ultrasonic ranger topic here
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected to server (i.e., broker) with result code " + str(rc))
+
+    client.subscribe(serialIDACK)
+    client.message_callback_add(serialIDACK, on_generation_ACK)
+
+    """
+    #subscribe to the ultrasonic ranger topic here
     client.subscribe("parkingNode/ultrasonicRanger") #Maxc
     client.message_callback_add("parkingNode/ultrasonicRanger", on_Ranger) #Maxc
 
@@ -18,6 +31,7 @@ def on_connect(client, userdata, flags, rc):
     # subscribe to the email topic here
     client.subscribe("parkingNode/email") #Maxc
     client.message_callback_add("parkingNode/email", on_Email) #Maxc
+    """
 
 
 #D efault message callback. Please use custom callbacks.
@@ -26,9 +40,22 @@ def on_message(client, userdata, msg):
 
 def on_Ranger(client, userdata, message):    
     # Action for Ranger message is printing out the value.
-    print("RangeFinder Value: " + str(message.payload, "utf-8") + " cm") #Maxc
+    print("RangeFinder Value: " + str(message.payload, "utf-8") + " cm")
 
+def on_generation_ACK(client, userdata, message):
+    # action for recieving confirmation that serial ID is assigned
+    global serialID
+    global isSerialOpenForInit
+    
+    # code to check the ACK serialID, and increment it by one
+    print("Current Serial ID: " + str(serialID))
+    ack_message = str(message.payload, "utf-8")
+    print("ACK Message: " + ack_message)
+    serialID += 1
+    print("New Serial ID: " + str(serialID))
+    isSerialOpenForInit = True
 
+"""
 def on_Button(client, userdata, message):  
     # Action for Button message is printing out if the button pressed.
     if (str(message.payload, "utf-8") == 'Y'):
@@ -39,7 +66,7 @@ def on_Button(client, userdata, message):
 def on_Email(client, userdata, message):     
     # Action for email message is printing out the string.
     print("Received email info:" + str(message.payload, "utf-8") ) #Maxc
-
+"""
 
 if __name__ == '__main__':
     #this section is covered in publisher_and_subscriber_example.py
@@ -50,4 +77,7 @@ if __name__ == '__main__':
     client.loop_start()
 
     while True:
-        time.sleep(1)
+        if(isSerialOpenForInit):
+            client.publish(serialID_gen, str(serialID))
+            isSerialOpenForInit = False
+        time.sleep(.1)
