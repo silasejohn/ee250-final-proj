d7b5a181507f17bdb570cfe10c5a0c30eca3e0de
Silas John <sejohn@usc.edu>
2022-05-02 16:26:42 +0000

    re commented vm_subndoe

--- vm_subnode.py
+++ vm_subnode.py
@@ -7,45 +7,38 @@ import time
 def on_connect(client, userdata, flags, rc):
     print("Connected to server (i.e., broker) with result code "+str(rc))
 
-    #subscribe to the ultrasonic ranger topic here
+    #s ubscribe to the ultrasonic ranger topic here
     client.subscribe("parkingNode/ultrasonicRanger") #Maxc
     client.message_callback_add("parkingNode/ultrasonicRanger", on_Ranger) #Maxc
 
-    #subscribe to the button topic here
+    # subscribe to the button topic here
     client.subscribe("parkingNode/button") #Maxc
     client.message_callback_add("parkingNode/button", on_Button) #Maxc
 
-    #subscribe to the email topic here
+    # subscribe to the email topic here
     client.subscribe("parkingNode/email") #Maxc
     client.message_callback_add("parkingNode/email", on_Email) #Maxc
 
 
-#Default message callback. Please use custom callbacks.
+#D efault message callback. Please use custom callbacks.
 def on_message(client, userdata, msg):
     print("on_message: " + msg.topic + " " + str(msg.payload, "utf-8"))
 
-def on_Ranger(client, userdata, message): #Maxc rename from suctom_callback()
-    #the third argument is 'message' here unlike 'msg' in on_message
-    
+def on_Ranger(client, userdata, message):    
     # Action for Ranger message is printing out the value.
-    print("VM: " + str(message.payload, "utf-8") + " cm") #Maxc
+    print("RangeFinder Value: " + str(message.payload, "utf-8") + " cm") #Maxc
 
 
-def on_Button(client, userdata, message): #Maxc rename from suctom_callback()
-    #the third argument is 'message' here unlike 'msg' in on_message
-    
+def on_Button(client, userdata, message):  
     # Action for Button message is printing out if the button pressed.
-    print("VM: " + str(message.payload, "utf-8") ) #Maxc
     if (str(message.payload, "utf-8") == 'Y'):
         print("Button pressed!")
     else:
         print("Button not pressed!")
 
-def on_Email(client, userdata, message): #Maxc rename from suctom_callback()
-    #the third argument is 'message' here unlike 'msg' in on_message
-    
+def on_Email(client, userdata, message):     
     # Action for email message is printing out the string.
-    print("Received email Info:" + str(message.payload, "utf-8") ) #Maxc
+    print("Received email info:" + str(message.payload, "utf-8") ) #Maxc
 
 
 if __name__ == '__main__':
@@ -57,6 +50,4 @@ if __name__ == '__main__':
     client.loop_start()
 
     while True:
-        #print("delete this line")
-        #print("Waiting for the subscribed topic ...")
         time.sleep(1)
