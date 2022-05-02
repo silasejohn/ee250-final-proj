0cd01fd887009e005969362aeeedc7462357f82e
Silas John <sejohn@usc.edu>
2022-05-02 08:56:43 -0700

    Create vm_pubnode.py

---
+++ vm_pubnode.py
@@ -0,0 +1,47 @@
+"""EE 250L Final Project Code
+Run vm_publisher-maxcProj.py in a separate terminal on your VM."""
+
+import paho.mqtt.client as mqtt
+import time
+from pynput import keyboard
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected to server (i.e., broker) with result code "+str(rc))
+
+    #subscribe to topics of interest here
+
+#Default message callback. Please use custom callbacks.
+def on_message(client, userdata, msg):
+    print("on_message: " + msg.topic + " " + str(msg.payload, "utf-8"))
+
+def on_press(key):
+    try: 
+        k = key.char # single-char keys
+    except: 
+        k = key.name # other keys
+    
+    if k == 'm':
+        print("Add money publised")
+        # send 10 cent to rpi
+        client.publish("xchen335/lcd", "m")
+        # send "LED_OFF"
+        client.publish("xchen335/credit", "10")
+
+
+if __name__ == '__main__':
+    #setup the keyboard event listener
+    lis = keyboard.Listener(on_press=on_press)
+    lis.start() # start to listen on a separate thread
+
+    #this section is covered in publisher_and_subscriber_example.py
+    client = mqtt.Client()
+    client.on_message = on_message
+    client.on_connect = on_connect
+    client.connect(host="eclipse.usc.edu", port=1883, keepalive=60)
+    client.loop_start()
+
+    while True:
+        #print("delete this line")
+        #print("Waiting for publishing...")
+        time.sleep(1)
+            
