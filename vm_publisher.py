322e91fe2918a095805e7f62a5421bb93ac54137
Silas John <sejohn@usc.edu>
2022-05-02 15:31:59 +0000

    Changed port numbers

--- vm_publisher.py
+++ vm_publisher.py
@@ -51,7 +51,7 @@ if __name__ == '__main__':
     client = mqtt.Client()
     client.on_message = on_message
     client.on_connect = on_connect
-    client.connect(host="eclipse.usc.edu", port=11000, keepalive=60)
+    client.connect(host="eclipse.usc.edu", port=1883, keepalive=60)
     client.loop_start()
 
     while True:
