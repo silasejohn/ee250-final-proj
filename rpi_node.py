fcc46d67dd8d11695826be7442f57407ce995fc9
Silas John <sejohn@usc.edu>
2022-05-03 00:17:06 +0000

    fix minor visual issues

--- rpi_node.py
+++ rpi_node.py
@@ -214,6 +214,7 @@ if __name__ == '__main__':
                 totalMoneyInserted = 0 
             timeLeft = int(timeAllotted) - int(timeDiff)
             displayText = "Time Left: \n" + str(timeLeft)
+            setText_norefresh(displayText)
             # display time left
         elif (nodeState is "EMPTY"):
             digitalWrite(ledR,0)
@@ -222,9 +223,12 @@ if __name__ == '__main__':
             displayText = "Clearing Extra\nMoney . . ."
             setText(displayText)
             time.sleep(3)
+            pubtopic_nodeMoneyInserted = nodeName + "/nodeMoneyInserted"
+            client.publish(pubtopic_nodeMoneyInserted, node_serialID + ":" + str(totalMoneyInserted))
+            print("Published Topic: " + pubtopic_nodeMoneyInserted + " with a message of " + str(totalMoneyInserted))
             totalMoneyInserted = 0
         
-        if (previousText is not displayText):
+        if ((previousText is not displayText) and (nodeState is not "SAFE")):
             setText(displayText)
 
         previousText = displayText
