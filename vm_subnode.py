8969c3e10a0a4595d7fe124cb20e327096e7de3e
Silas John <sejohn@usc.edu>
2022-05-04 22:37:32 +0000

    started developing a table

--- vm_subnode.py
+++ vm_subnode.py
@@ -8,6 +8,61 @@
 
 import paho.mqtt.client as mqtt
 import time
+import sqlite3
+
+"""
+CREATE TABLE sharks(id integer NOT NULL, 
+                    name text NOT NULL, 
+                    sharktype text NOT NULL, 
+                    length integer NOT NULL);
+
+Column Names:
+id 
+name 
+sharktype
+length
+
+INSERT INTO sharks VALUES (1, "Sammy", "Greenland Shark", 427);
+
+VIEW TABLES
+SELECT * FROM sharks; // view whole table
+
+SELECT * FROM sharks WHERE id IS 1; // display column where id = 1
+
+ALTER TABLE sharks ADD COLUMN age integer; // add a column to the table 
+
+UPDATE sharks SET age = 272 WHERE id=1; // set the age value for id = 1, shark table
+
+DELETE FROM sharks WHERE age <= 200; // delete every row for this is true
+
+CREATE TABLE endangered (id integer NOT NULL, status text NOT NULL);
+INSERT INTO endangered VALUES (1,  "near threatened");
+
+
+?? CODE ??
+
+import sqlite3
+
+connection = sqlite3.connect('database.db')
+
+
+with open('schema.sql') as f:
+    connection.executescript(f.read())
+
+cur = connection.cursor()
+
+cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+            ('First Post', 'Content for the first post')
+            )
+
+cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+            ('Second Post', 'Content for the second post')
+            )
+
+connection.commit()
+connection.close()
+
+"""
 
 #################
 ## Global Vars ##
